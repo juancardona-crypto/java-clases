9f0bad9178b09da2335363cfb5b56af23f46d32c
Juan Diego Cardona <juancardona1611@gmail.com>
2026-02-18 10:06:20 -0500

    constructores, getter y setter

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -1,10 +1,72 @@
 package com.example;
 
 public class Book {
-    String nombre;
-    int stock;
-    boolean enventa;
-    double precio;
+    private String nombre;
+    private int stock;
+    private boolean enventa;
+    private double precio;
+    
+    public Book (){
+        this.nombre = null;
+        this.stock = 0;
+        this.enventa = false;
+        this.precio = 0.0;
+        
+    }
+    public Book (String nombre, int stock, boolean enventa, double precio){
+        this.nombre = nombre;
+        this.stock = stock;
+        this.enventa = enventa;
+        this.precio = precio;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrcio(double precio){
+        if (precio >= 0){
+            this.stock = stock;
+        }
+        else{
+            System.out.println("Precio incorrecto");
+        }
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock){
+        if (stock >= 0){
+            this.stock = stock;
+        }
+        else{
+            System.out.println("Cantidad invalida");
+        }
+    }
+
+    public boolean isEnventa(){
+        return enventa;
+    }
+
+    public void setEnventa(boolean enventa) {
+        this.enventa = enventa;
+    }
+
     
 
+
+
 }
+
